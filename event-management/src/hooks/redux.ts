6d90b5f4d12f4a0f9386c06d673426493579ae16
Victor Hugo Almeida de Almeida <victorhugoalmeidadealmeida@MacBook-Pro-de-Victor.local>
2025-11-23 17:48:28 -0300

    feat: add hooks redux tipado para dispatch e selector

---
+++ event-management/src/hooks/redux.ts
@@ -0,0 +1,12 @@
+// src/hooks/redux.ts
+"use client";
+
+import {
+  TypedUseSelectorHook,
+  useDispatch,
+  useSelector,
+} from "react-redux";
+import type { RootState, AppDispatch } from "@/store/store";
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
